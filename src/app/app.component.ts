f952987acd1763e3b0da07a58baa7414420e0c85
song_gld <381471766@qq.com>
2022-02-01 15:36:52 +0800

    sss

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+// Copyright @ 2018-2021 xiejiahe. All rights reserved. MIT license.
+// See https://github.com/xjh22222228/nav
+
+import { Component } from '@angular/core'
+import { Router, ActivatedRoute } from '@angular/router'
+import { queryString, setLocation } from '../utils'
+import { en_US, NzI18nService, zh_CN } from 'ng-zorro-antd/i18n'
+import { getLocale } from 'src/locale'
+
+@Component({
+  selector: 'app-xiejiahe',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor (private router: Router, private activatedRoute: ActivatedRoute, private i18n: NzI18nService) {}
+
+  ngOnInit() {
+    this.goRoute()
+    this.activatedRoute.queryParams.subscribe(setLocation)
+
+    if (getLocale() === 'zh-CN') {
+      this.i18n.setLocale(zh_CN);
+    } else {
+      this.i18n.setLocale(en_US);
+    }
+  }
+
+  goRoute() {
+    // is App
+    if ('ontouchstart' in window) {
+      const url = (this.router.url.split('?')[0] || '').toLowerCase()
+      const { page, id, q } = queryString()
+      const queryParams = { page, id, q }
+
+      if (!url.includes('/app')) {
+        this.router.navigate(['/app'], { queryParams })
+      }
+    }
+  }
+}
