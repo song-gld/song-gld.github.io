109e0f3f30c76d8cd6fcb18cac954a11b8f71319
song-gld <381471766@qq.com>
2023-01-28 19:46:21 +0800

    edit the css

--- js/news.js
+++ js/news.js
@@ -5,19 +5,22 @@ $(function(){
            $('#news').append('<li>' + data.data[i].title + '</li>') 
         }
     })
-    // $.get("https://www.mxnzp.com/api/history/today?type=1&app_id=ipxswkmoqiszlwzm&app_secret=QjlCV3duZkQ2NkVEN1VHNmNyTU5KUT09",function(data,status){
-    //     console.log(data)
-    //     for(var i =0; i < 11; i++) {
-    //        $('#todays').append('<li>' + data.data[i].title + '</li>') 
-    //     }
-    // })  
-    $.ajax({ 
-        url:"https://www.mxnzp.com/api/history/today?type=1&app_id=ipxswkmoqiszlwzm&app_secret=QjlCV3duZkQ2NkVEN1VHNmNyTU5KUT09", 
-        type:'GET', 
-        dataType:'JSONP',  // 处理Ajax跨域问题
-        success: function(data){ 
-            console.log("ajax:" + data) 
-        } 
-    });
+    setTimeout(getL,2000)
+        // $.ajax({ 
+    //     url:"https://www.mxnzp.com/api/history/today?type=1&app_id=ipxswkmoqiszlwzm&app_secret=QjlCV3duZkQ2NkVEN1VHNmNyTU5KUT09", 
+    //     type:'GET', 
+    //     dataType:'JSONP',  // 处理Ajax跨域问题
+    //     success: function(data){ 
+    //         console.log("ajax:" + data) 
+    //     } 
+    // });
 })
+function getL(){
+   $.get("https://www.mxnzp.com/api/history/today?type=1&app_id=ipxswkmoqiszlwzm&app_secret=QjlCV3duZkQ2NkVEN1VHNmNyTU5KUT09",function(data,status){
+            console.log(data)
+            for(var i =0; i < 11; i++) {
+               $('#todays').append('<li>' + data.data[i].title + '</li>') 
+            }
+        })   
+}
 
